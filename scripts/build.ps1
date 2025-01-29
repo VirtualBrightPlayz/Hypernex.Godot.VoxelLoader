1fb9319fe65fbb76c80d515dcd338cdb2aceebf5
VirtualBrightPlayz <lego.trains.b@gmail.com>
2025-01-29 14:36:39 -0700

    update build script on windows

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,10 +1,5 @@
 dotnet build -c Debug
 
-Copy-Item -Path .godot/mono/temp/bin/Debug/AssetRipper.TextureDecoder.dll -Destination $env:APPDATA/Godot/app_userdata/Hypernex.Godot/Plugins -Force
-Copy-Item -Path .godot/mono/temp/bin/Debug/AssetsTools.NET.dll -Destination $env:APPDATA/Godot/app_userdata/Hypernex.Godot/Plugins -Force
-Copy-Item -Path .godot/mono/temp/bin/Debug/AssetsTools.NET.Texture.dll -Destination $env:APPDATA/Godot/app_userdata/Hypernex.Godot/Plugins -Force
-Copy-Item -Path .godot/mono/temp/bin/Debug/Fmod5Sharp.dll -Destination $env:APPDATA/Godot/app_userdata/Hypernex.Godot/Plugins -Force
-Copy-Item -Path .godot/mono/temp/bin/Debug/Hypernex.Godot.UnityLoader.dll -Destination $env:APPDATA/Godot/app_userdata/Hypernex.Godot/Plugins -Force
-Copy-Item -Path .godot/mono/temp/bin/Debug/Hypernex.Godot.UnityLoader.pdb -Destination $env:APPDATA/Godot/app_userdata/Hypernex.Godot/Plugins -Force
-Copy-Item -Path .godot/mono/temp/bin/Debug/IndexRange.dll -Destination $env:APPDATA/Godot/app_userdata/Hypernex.Godot/Plugins -Force
-Copy-Item -Path .godot/mono/temp/bin/Debug/OggVorbisEncoder.dll -Destination $env:APPDATA/Godot/app_userdata/Hypernex.Godot/Plugins -Force
+Copy-Item -Path .godot/mono/temp/bin/Debug/Hypernex.Godot.VoxelLoader.dll -Destination $env:APPDATA/Godot/app_userdata/Hypernex.Godot/Plugins -Force
+Copy-Item -Path .godot/mono/temp/bin/Debug/Hypernex.Godot.VoxelLoader.pdb -Destination $env:APPDATA/Godot/app_userdata/Hypernex.Godot/Plugins -Force
+Copy-Item -Path .godot/mono/temp/bin/Debug/SharpNBT.dll -Destination $env:APPDATA/Godot/app_userdata/Hypernex.Godot/Plugins -Force
